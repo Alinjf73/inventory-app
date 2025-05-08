63a1699fa2f0356c863a8f69f05181016c94f8c5
Alinjf73 <129929026+Alinjf73@users.noreply.github.com>
2025-05-08 22:38:23 +0330

    Update AppHeader.jsx

--- src/UI/AppHeader.jsx
+++ src/UI/AppHeader.jsx
@@ -8,7 +8,7 @@ function AppHeader() {
   const productCount = useMemo(() => products?.length ?? 0, [products]);
 
   return (
-    <div className=" bg-secondary-400 text-secondary-900 flex items-center justify-center gap-x-2 py-2 font-bold text-lg mb-10">
+    <div className=" bg-secondary-400 text-secondary-900 flex items-center justify-center gap-x-2 p-2 font-bold md:text-lg  mb-10">
       <h1>Inventory App using tailwind & React.js</h1>
       <Count productCount={productCount} />
       <DarkModeToggle />
